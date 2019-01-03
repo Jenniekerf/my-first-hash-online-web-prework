5f7ddb58ae142212a73e18a90c836d813022c1c7
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-03 18:32:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,30 +1,4 @@
-def my_hash 
-  your_hash = {"cat" => "Jan", "my name" => "Jennie"}
-end
-
-
-def shipping_manifest
-  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  end
-
-
-def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
-  shipping_manifest["oil paintings"]
-end
-
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-  }
-shipping_manifest["muskets"] = 2
-shipping_manifest["gun powder"] = 4
-shipping_manifest
-end
+your_hash = {
+  cat => "Jan",
+  dog => "Finn"
+}
